f336fa8be084397e7c423954150be6a760db84fb
andrenator23 <128236929+andrenator23@users.noreply.github.com>
2025-03-21 01:15:18 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const CONTRACT_ADDRESS = "0xYourContractAddressHere";
+
+export const CONTRACT_ABI = [
+    {
+        "inputs": [
+            { "internalType": "string", "name": "_tokenURI", "type": "string" }
+        ],
+        "name": "mint",
+        "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
